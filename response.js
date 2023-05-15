ffd4cb2fbb7612319db479021ae75a976dd3afe5
unknown <jaskirat2357@gmail.com>
2023-05-16 02:20:21 +0530

    chat

---
+++ response.js
@@ -0,0 +1,13 @@
+const responseObj={
+    hello:"Hey ! How are you doing?",
+    hey:"Hey! What's Up",
+    whatisapplicationformdeadlineforvariousexams:"Please, search by name",
+    NEST :'01-04-2023',
+    IISERIAT:'23-03-2023',
+   CEBS :"17-10-2022",
+   IATSCB:"27-09-2022",
+   NESTSMAS:"21-12-2022",
+   TVISHA:"02-03-2023",
+   IITM:"12-05-2023",
+  
+}
